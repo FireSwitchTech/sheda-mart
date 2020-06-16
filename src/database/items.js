dcaa4bab628f0f7ef7f8042933d37a747b2ca9bd
unknown <temiloluwaabiodunojo@gamail.com>
2020-06-16 14:33:28 +0100

    array of categories added

---
+++ src/database/items.js
@@ -0,0 +1,59 @@
+import img1 from './img/cam5.jpg'
+import img2 from './img/cam6.jpg'
+import img3 from './img/cam7.jpg'
+import img4 from './img/cam8.jpg'
+import img5 from './img/drone1.jpg'
+import img6 from './img/lens1.jpg'
+import img7 from './img/reflector.jpg'
+import img8 from './img/cam3.jpg'
+const items =[
+    {
+        id:1,
+        dp:img1,
+        name:'Digital Video Camera Video Camcorder HD 1080P...',
+        price:'11,000'
+    },
+    {
+        id:2,
+        dp:img2,
+        name:'Nikon Nikon D850 DSLR Camera (Body Only)',
+        price:'1,000,000'
+    },
+    {
+        id:3,
+        dp:img3,
+        name:'Nikon D850 DSLR Camera (Body Only)',
+        price:'12,000'
+    },
+    {
+        id:4,
+        dp:img4,
+        name:'Dji Mavic 2 Zoom Drone Quadcopter',
+        price:'11,999'
+    },
+    {
+        id:5,
+        dp:img5,
+        name:'Cam stand',
+        price:'7,000'
+    },
+    {
+        id:6,
+        dp:img6,
+        name:'Cam stand',
+        price:220000
+    },
+    {
+        id:7,
+        dp:img7,
+        name:'Cam stand',
+        price:12000
+    },
+    {
+        id:8,
+        dp:img8,
+        name:'Cam stand',
+        price:12000
+    },
+]
+export default items;
